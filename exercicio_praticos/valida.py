63df108d8a7387b68f68139c85073d889a6450f6
endisoares <endi.soares89@gmail.com>
2022-06-20 07:52:07 -0100

    update

--- exercicio_praticos/valida.py
+++ exercicio_praticos/valida.py
@@ -1,8 +1,16 @@
-def menu():
-    print('Especificação   Código  Preço\n'
-          'Cachorro Quente 100     R$ 1,20\n'
-          'Bauru Simples   101     R$ 1,30\n'
-          'Bauru com ovo   102     R$ 1,50\n'
-          'Hambúrguer      103     R$ 1,20\n'
-          'Cheeseburguer   104     R$ 1,30\n'
-          'Refrigerante    105     R$ 1,00')
+def digitefloat(n='Digite um número real: '):
+    """
+    Função que aceita somente números reais (float), aceita números com vírgula.
+    :param n: informar o número.
+    :return: Retorna um valor real.
+    """
+    while True:
+        try:
+            num = str(input(n)).strip().replace(',', '.')
+            final = float(num)
+            return final
+        except ValueError:
+            print('Digite sua altura em números separados por vírgula!')
+            continue
+        except Exception as erro:
+            print(f'Infelizmente tivemos o erro: {erro.__class__}')
